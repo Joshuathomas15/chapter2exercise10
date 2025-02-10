84eeb6f72d7cd32c1e4862424be716f5ccf149c2
Joshuathomas15 <jjulyo6mdls958@gmail.com>
2025-02-10 17:36:49 -0500

    Add project files.

---
+++ chapter2exercise10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    double milesDriven = 476,
+        gallonsofgasused = 25,
+        MPG = milesDriven / gallonsofgasused;
+    cout << endl
+        << "miles driven = " << milesDriven << endl
+        << "Gallons used =" << gallonsofgasused << endl
+        << " MPG = " << MPG << endl
+        << endl;
+
+    return 0;
+}
